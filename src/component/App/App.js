c6a8e10e50fb631ab6596932216a6af6707dade8
kevinmsavsani <kevinmsavsani@gmail.com>
2020-12-25 17:46:20 +0530

    Timer component added

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import Timer from '../Timer/Timer';
 import './App.css';
 
-const App = () => <div className="app-container" />;
+const App = () => <div className="app-container">    
+    <Timer />
+</div>;
 
 export default App;
