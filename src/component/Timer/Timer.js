ee4af8c5fae986ff0109f56d7fa81fcfb15f2263
kevinmsavsani <kevinmsavsani@gmail.com>
2020-12-25 17:53:05 +0530

    TimerButton Test added

--- src/component/Timer/Timer.js
+++ src/component/Timer/Timer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Timer.css';
 
 class Timer extends Component {
   constructor(props) {
---
+++ src/component/TimerButton/TimerButton.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import './TimerButton.css';
+
+class TimerButton extends Component {
+
+}
+
+export default TimerButton;
